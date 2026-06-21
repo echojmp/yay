925fac39bbdf008eabd01f1186e2f9926e15c03f
echojmp <wyatt.bastide.3@gmail.com>
2026-06-21 22:15:56 +0200

    smelt function

---
+++ src/main/java/echojmp/yay/Utils.java
@@ -0,0 +1,45 @@
+package echojmp.yay;
+
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.*;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.world.World;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class Utils {
+    private static final Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.createFuelTimeMap();
+
+    // same thing as in the thing
+    public static int getFuelTime(ItemStack fuel) {
+        if (fuel.isEmpty()) {
+            return 0;
+        }
+
+        Item item = fuel.getItem();
+        return fuelMap.getOrDefault(item, 0);
+    }
+
+    // returns a new ItemStack that the given ItemStack would become if fully smelted in a furnace
+    public static ItemStack smelt(World world, ItemStack item) {
+        RecipeManager recipeManager = world.getRecipeManager();
+
+        Inventory inventory = new SimpleInventory(1);
+        inventory.setStack(0, item);
+        Optional<SmeltingRecipe> recipe = recipeManager.getFirstMatch(RecipeType.SMELTING, inventory, world);
+
+        if (recipe.isPresent()) {
+            ItemStack smeltResult = recipe.get().getOutput(world.getRegistryManager());
+            smeltResult.setCount(item.getCount());
+            return smeltResult;
+        }
+        return ItemStack.EMPTY;
+    }
+}
--- src/main/java/echojmp/yay/Yay.java
+++ src/main/java/echojmp/yay/Yay.java
@@ -3,12 +3,18 @@ package echojmp.yay;
 import echojmp.yay.Enchants.Smelt;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class Yay implements ModInitializer {
 	public static final String MOD_ID = "yay";
@@ -16,7 +22,20 @@ public class Yay implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		// enchants
 		Registry.register(Registries.ENCHANTMENT, new Identifier(MOD_ID, "smelt"), new Smelt());
+
+		// Commands
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("smelt")
+		.executes(context -> {
+			ServerPlayerEntity player = context.getSource().getPlayer();
+			ItemStack mainItem = player.getMainHandStack();
+
+			ItemStack result = echojmp.yay.Utils.smelt(context.getSource().getWorld(), player.getMainHandStack());
+			player.getInventory().setStack(player.getInventory().selectedSlot, result);
+
+			return 1;
+		})));
 	}
 
 	public static Identifier id(String path) {
