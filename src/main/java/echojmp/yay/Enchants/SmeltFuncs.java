0f4ed0340864c65cbbafdca877692accddff1e0e
echojmp <wyatt.bastide.3@gmail.com>
2026-06-22 16:20:43 +0200

    fix?

--- src/main/java/echojmp/yay/Enchants/SmeltFuncs.java
+++ src/main/java/echojmp/yay/Enchants/SmeltFuncs.java
@@ -42,57 +42,56 @@ public class SmeltFuncs {
             int smeltLevel = EnchantmentHelper.getLevel(Yay.ENCHANTMENT_SMELT, player.getMainHandStack());
 
             if (smeltLevel > 0) {
-                // this needs to run on the client I think
-                Yay.LOGGER.info("playing sound");
-                world.playSound(player, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                if (world.isClient()) {
+                    // this needs to run on the client I think?
+                    Yay.LOGGER.info("playing sound");
+                    world.playSound(player, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 1.0F, 1.0F);
 
-                Yay.LOGGER.info("showing particles");
-                Random rand = Random.create();
-                for (int i = 0; i < 8; i++) {
-                    world.addParticle(ParticleTypes.ASH,
-                            pos.getX() + (rand.nextFloat() - .5f),
-                            pos.getY() + (rand.nextFloat() - .5f),
-                            pos.getZ() + (rand.nextFloat() - .5f),
-                            0.0D + (rand.nextFloat() - .5f) / 20 * 4,
-                            0.0D + (rand.nextFloat() - .15f) / 20 * 2,
-                            0.0D + (rand.nextFloat() - .5f) / 20 * 4
+                    Yay.LOGGER.info("showing particles");
+                    Random rand = Random.create();
+                    for (int i = 0; i < 8; i++) {
+                        world.addParticle(ParticleTypes.ASH,
+                                pos.getX() + (rand.nextFloat() - .5f),
+                                pos.getY() + (rand.nextFloat() - .5f),
+                                pos.getZ() + (rand.nextFloat() - .5f),
+                                0.0D + (rand.nextFloat() - .5f) / 20 * 4,
+                                0.0D + (rand.nextFloat() - .15f) / 20 * 2,
+                                0.0D + (rand.nextFloat() - .5f) / 20 * 4
+                        );
+                        world.addParticle(ParticleTypes.SMALL_FLAME,
+                                pos.getX() + (rand.nextFloat() - .5f),
+                                pos.getY() + (rand.nextFloat() - .5f),
+                                pos.getZ() + (rand.nextFloat() - .5f),
+                                0.0D + (rand.nextFloat() - .5f) / 20 * 4,
+                                0.0D + (rand.nextFloat() - .15f) / 20 * 2,
+                                0.0D + (rand.nextFloat() - .5f) / 20 * 4
+                        );
+                    }
+                } else {
+                    Yay.LOGGER.info("drops");
+                    List<ItemEntity> drops = world.getEntitiesByClass(ItemEntity.class,
+                            new Box(pos),
+                            drop -> {return true;}
                     );
-                    world.addParticle(ParticleTypes.SMALL_FLAME,
-                            pos.getX() + (rand.nextFloat() - .5f),
-                            pos.getY() + (rand.nextFloat() - .5f),
-                            pos.getZ() + (rand.nextFloat() - .5f),
-                            0.0D + (rand.nextFloat() - .5f) / 20 * 4,
-                            0.0D + (rand.nextFloat() - .15f) / 20 * 2,
-                            0.0D + (rand.nextFloat() - .5f) / 20 * 4
-                    );
-                }
+                    Yay.LOGGER.info("found " + drops.size() + " drops");
 
-
-                Yay.LOGGER.info("drops");
-                List<ItemEntity> drops = world.getEntitiesByClass(ItemEntity.class,
-                        new Box(pos),
-                        drop -> {return true;}
-                );
-
-                // aaaaaaa
-                Yay.LOGGER.info("found " + drops.size() + " drops");
-
-                int fuelEff = Smelt.getFuelEfficiency(player.getOffHandStack());
-                Map<ItemEntity, Integer> fuelMap = Maps.newLinkedHashMap();
-                while (fuelEff > 0) {
-                    for (ItemEntity drop : drops) {
-                        fuelMap.put(drop, fuelMap.getOrDefault(drop, 0) + 1);
-                        fuelEff -= 1;
-                        if (fuelEff == 0) {
-                            break;
+                    int fuelEff = Smelt.getFuelEfficiency(player.getOffHandStack());
+                    Map<ItemEntity, Integer> fuelMap = Maps.newLinkedHashMap();
+                    while (fuelEff > 0) {
+                        for (ItemEntity drop : drops) {
+                            fuelMap.put(drop, fuelMap.getOrDefault(drop, 0) + 1);
+                            fuelEff -= 1;
+                            if (fuelEff == 0) {
+                                break;
+                            }
                         }
                     }
-                }
 
-                for (Map.Entry<ItemEntity, Integer> entry : fuelMap.entrySet()) {
-                    int fuel = entry.getValue();
-                    ItemEntity drop = entry.getKey();
-                    Yay.LOGGER.info("got " + fuel + " fuel for " + drop);
+                    for (Map.Entry<ItemEntity, Integer> entry : fuelMap.entrySet()) {
+                        int fuel = entry.getValue();
+                        ItemEntity drop = entry.getKey();
+                        Yay.LOGGER.info("got " + fuel + " fuel for " + drop);
+                    }
                 }
             }
         });
